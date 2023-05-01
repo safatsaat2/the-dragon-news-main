3cc89103ead1f2fed4d495b3e2ac287ffe114155
safatsaat2 <sakibsafat47@gmail.com>
2023-05-01 14:44:50 +0600

    nav done

--- src/pages/shared/Header/Header.jsx
+++ src/pages/shared/Header/Header.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import logo from '../../../assets/logo.png'
 import moment from 'moment/moment';
-import { Button, Container } from 'react-bootstrap';
+import { Button, Container, Nav, NavDropdown, Navbar } from 'react-bootstrap';
+import Marquee from "react-fast-marquee";
+
 
 const Header = () => {
     return (
@@ -11,9 +13,32 @@ const Header = () => {
                 <p><small>Journalism Without Fear or Favour</small></p>
                 <p>{moment().format('dddd, MMMM, Do, YYYY')}</p>
             </div>
-            <div>
-            <Button variant="danger">Danger</Button>{' '}
+            <div className='d-flex'>
+                <Button variant="danger">Danger</Button>
+                <Marquee className='text-danger' speed={100}>
+                    Manila airport cancels 40 domestic flights after power outage.
+                    Air defence systems repelling Russian missile attacks in Kyiv.
+                    Thai PM candidate gives birth two weeks before election.
+                </Marquee>
             </div>
+            <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+                <Container>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mx-auto">
+                            <Nav.Link href="#features">Home</Nav.Link>
+                            <Nav.Link href="#pricing">About</Nav.Link>
+                            <Nav.Link href="#pricing">Career</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#deets">Profile</Nav.Link>
+                            <Nav.Link eventKey={2} href="#memes">
+                            <Button variant="secondary">Log in</Button>
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </Container>
     );
 };
