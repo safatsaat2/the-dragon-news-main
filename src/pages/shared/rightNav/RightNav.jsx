1c732a33ee9f6bfb26a073357f2ede43bee470a6
safatsaat2 <sakibsafat47@gmail.com>
2023-05-01 15:01:12 +0600

    right nav done

--- src/pages/shared/rightNav/RightNav.jsx
+++ src/pages/shared/rightNav/RightNav.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Button, ListGroup } from 'react-bootstrap';
+import { FaGooglePlusG, FaGithub, FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa';
+
 
 const RightNav = () => {
     return (
         <div>
-            <h2>Right Nav</h2>
+            <h4>Log in with</h4>
+            <Button className='mb-2' variant="outline-primary"> <FaGooglePlusG /> Log in with Google</Button>
+            <Button className='mb-2' variant="outline-secondary"> <FaGithub /> Log in with Github</Button>
+
+            <div>
+                <h4>Find Us</h4>
+                <ListGroup>
+                    <ListGroup.Item> <FaFacebook /> FaceBook</ListGroup.Item>
+                    <ListGroup.Item><FaInstagram /> Instagram</ListGroup.Item>
+                    <ListGroup.Item><FaTwitter /> Twitter</ListGroup.Item>
+                </ListGroup>
+            </div>
         </div>
     );
 };
