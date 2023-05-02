b48ffe0806177a52920908ad0739c20b7e5d1583
safatsaat2 <sakibsafat47@gmail.com>
2023-05-02 11:47:43 +0600

    Editor Insight

--- src/pages/news/News.jsx
+++ src/pages/news/News.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Button, Card } from 'react-bootstrap';
+import { FaArrowLeft } from 'react-icons/fa';
+import { Link, useLoaderData } from 'react-router-dom';
+import EditorInsight from './EditorInsight';
 
 const News = () => {
+    const news = useLoaderData()
+    const { _id, title, details, image_url, category_id } = news
     return (
         <div>
-            <h2>News Details Coming soon...</h2>
+            <Card>
+                <Card.Img variant="top" src={image_url} />
+                <Card.Body>
+                    <Card.Title>{title}</Card.Title>
+                    <Card.Text>
+                        {details}
+                    </Card.Text>
+                    <Link to={`/categories/${category_id}`}><Button variant="danger"> <FaArrowLeft></FaArrowLeft> All news in this category</Button>
+                    </Link>
+                </Card.Body>
+            </Card>
+            <EditorInsight/>
         </div>
     );
 };
